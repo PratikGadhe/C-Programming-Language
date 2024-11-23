63f8bb5ca699bebc48836b19cd9bde4d862ae9da
Pratik Vilas Gadhe <pratikgadhe366@gmail.com>
2024-11-23 16:33:41 +0530

    Delete Problem Practice Day 1 directory

--- Problem Practice Day 1/01_problem.c
+++
@@ -1,6 +0,0 @@
-//Write a program to print a string in C language
-#include <stdio.h>
-int main(){
-    printf("Hello world !!");
-    return 0;
-}
--- Problem Practice Day 1/02_problem2.c
+++
@@ -1,9 +0,0 @@
-// Write a program to accept values of two numbers and print their addition
-
-#include <stdio.h>
-int main(){
-    int a=12;
-    int b=10;
-    printf("The sum of two numbers is %d",a+b);
-    return 0;
-}
